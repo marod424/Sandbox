49988afac6118691c816fc63a4c491182694edd1
Earth <marod424@gmail.com>
2015-11-21 11:09:58 -0500

    Painted home page

--- index.php
+++ index.php
@@ -1,45 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="no-js">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>My Sandbox</title>
-    <style>
-        h1 { text-align: center; }
-        ul {
-            list-style: none;
-            background-color: rgb(244, 203, 141);
-            border: 2px solid rgb(211, 138, 75);
-            padding-bottom: 1em;
-        }
-        li a { text-decoration: none; }
-    </style>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/nav-menu.css">
+
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 <body>
-    <h1>Sand <i class="fa fa-cube fa-2x"></i>!</h1>
-    <ul>
-        <h3>PHP</h3>
-        <li><a href="AgileDesignPatterns/index.php">Agile Design Patterns</a></li>
-        <li><a href="arrays/index.php">Arrays</a></li>
-        <li><a href="conditionals/index.php">Conditionals</a></li>
-        <li><a href="cookies/index.php">Cookies</a></li>
-        <li><a href="formatted-strings/index.php">Formatted Strings</a></li>
-        <li><a href="functions/index.php">Functions</a></li>
-        <li><a href="heredocs/index.php">Heredocs</a></li>
-        <li><a href="includes/index.php">Includes</a></li>
-        <li><a href="loops/index.php">Loops</a></li>
-        <li><a href="objects/index.php">Objects</a></li>
-        <li><a href="phpSecurity/index.php">PHP Security</a></li>
-        <li><a href="$_post/index.php">POST</a></li>
-        <li><a href="sessions/index.php">Sessions</a></li>
-        <li><a href="super-globals/index.php">Super Globals</a></li>
-        <h3>Front End Design</h3>
-        <li><a href="AngularUI/index.html">AngularUI</a></li>
-        <li><a href="Boilerplate/index.html">Boilerplate</a></li>
-        <li><a href="css3/index.html">CSS3</a></li>
-        <li><a href="PatternLibrary/index.html">Pattern Library</a></li>
-        <li><a href="WebTemplate/index.html">Public Web Template</a></li>
-        <li><a href="Responsive/index.html">Responsive</a></li>
-    </ul>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+
+    <div id="nav-container" class="nav-container">
+        <nav id="nav-menu" class="nav-menu">
+            <ul class="nav-items">    
+                <li><a href="#" id="nav-trigger"><i class="fa fa-long-arrow-right fa-2x"></i>Sandbox<i class="fa fa-times fa-2x"></i></a></li>
+                <li><a href="app/Castles/index.html"><i class="fa fa-home fa-2x"></i>Castles</a></li>
+                <li><a href="app/php/index.html"><i class="fa fa-pied-piper fa-2x"></i>PHP</a></li>
+                <li><a href="app/design/index.html"><i class="fa fa-css3 fa-2x"></i>Design</a></li>
+            </ul>
+        </nav>
+
+        <div class="nav-pusher">
+            <div class="nav-content">
+                <h1>Sand <i class="fa fa-cube fa-2x"></i>!</h1>
+                <p>Lorem ipsum dolor sit amet, ea eos oratio delicata patrioque, ei maiorum perpetua mel. Ius ea odio patrioque. At mea velit tantas, usu modus saepe aeterno an. Has ne sint timeam vulputate, cibo vulputate efficiendi ut duo.
+                Nobis recteque sed in. Ea tantas option discere per, vix fugit dolore principes ut. Mutat torquatos no pro, alia probatus at cum. Autem sonet noster est cu, id vel sale summo consequat. Nam fugit nonumes appellantur in. Ea decore ubique quo.
+                Usu nonumy eirmod an. Has mentitum explicari temporibus in, eum ut zril feugiat gloriatur. Sea an doming definiebas, modus assum laoreet duo et, mel accumsan deseruisse no. Id animal percipit electram nec, sea audiam numquam te. Maluisset conclusionemque an eos. An viris albucius pro, ridens melius denique mei ut.
+                Phaedrum recteque pri et, magna reque tempor pri ut. Vim urbanitas rationibus eu, vim no mutat nostrum persecuti, in est idque interesset. Te atqui ceteros duo. Ad quodsi salutatus necessitatibus his, munere prompta suscipiantur mea no. Graeci ornatus expetenda an sit.
+                Regione verterem nam et, sit tale eirmod no. Vix recteque pericula splendide in, ne aeterno phaedrum eam. Nec dignissim scriptorem in, ei essent intellegebat has. Tantas feugiat complectitur cu mel, velit iudico eirmod eu cum. Eam at insolens expetendis constituam. Officiis voluptaria eos cu. In atomorum moderatius nam, in unum causae interesset vel.</p>
+            </div>
+        </div>
+    </div>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+    <script src="js/main.js"></script>
+    <script src="js/classie.js"></script>
+    <script src="js/sidebarEffects.js"></script>
 </body>
 </html>
